a70b298adaa556aa4bbef1341992832ffcf77ead
night-kriss <veseluhadima@gmail.com>
2024-11-11 23:56:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#Считывайте строки из файла (text.txt) 
+#и создайте новый файл (output.txt), заменяя каждую букву 'о' на 'a' в строках.
+with open(r"C:\Users\Lenovo\Desktop\Ипо\лр-5\6\text.txt", "r", encoding="utf-8") as file:
+      text = file.read()
+# Вывод содержимого файла на экран
+print("Содержимое исходного файла:")
+print(text)
+# Замена буквы 'о' на 'a'
+rtext=text.replace('а','о')
+# Запись изменённого текста в новый файл
+with open(r"C:\Users\Lenovo\Desktop\Ипо\лр-5\6\output.txt", "w", encoding="utf-8") as file:
+    file.write(rtext)
+# Вывод изменённого текста на экран
+print("Содержимое нового файла:")
+print(rtext)   
+    
+    
+    
